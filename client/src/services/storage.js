cfff73433b4ee81e28ac42b754fc01a7d34167ae
Vesper Gray Wetzel <deborah.wetzel@mykenzie.snhu.edu>
2023-04-05 14:11:49 -0600

    Storage Service complete

---
+++ client/src/services/storage.js
@@ -0,0 +1,20 @@
+class Storage {
+    searchResultsKey = "search-results"
+
+    getSearchResults() {
+       return this.get(this.searchResultsKey)   
+    } 
+
+    setSearchResults(value) {
+        this.set(this.searchResultsKey, value)
+    }
+
+
+    get(key) {
+        return JSON.parse(sessionStorage.getItem(key))
+    }
+
+    set(key, value) {
+        sessionStorage.setItem(key, JSON.stringify(value))
+    }
+}
